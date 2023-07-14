42b8ca842b69c9d5a77d6c7a2269ba9503420238
Atiwat Seenark <um_oom@hotmail.com>
2023-07-15 00:31:55 +0700

    add: fish shell and update kitty and zellij to use fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,124 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+###################### paths ######################
+fish_add_path /opt/homebrew/bin
+###################### paths ######################
+
+###################### starship ######################
+source (/opt/homebrew/bin/starship init fish --print-full-init | psub)
+###################### starship ######################
+
+
+###################### X Variable ######################
+# set -x NVV_DIR ~/.nvm
+###################### X Variable ######################
+
+###################### NVIM ######################
+# alias z1="zellij -l ~/.config/zellij/layouts/z1.kdl"
+function nl
+  env NVIM_APPNAME=nvim-lazy2 nvim
+end
+
+function z1 
+  command zellij -l ~/.config/zellij/layouts/z1.kdl $argv
+end
+# alias z2="zellij -l ~/.config/zellij/layouts/z2.kdl"
+function z2 
+  command zellij -l ~/.config/zellij/layouts/z2.kdl $argv
+end
+
+function nvims
+    set -l items "nvim-lazy2" "nvim-lazy" "chad-lazy" "default"
+    set -l config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+    
+    if test -z "$config"
+        echo "Nothing selected"
+        return 0
+    else if test "$config" = "default"
+        set config ""
+    end
+
+    env NVIM_APPNAME=$config nvim $argv
+end
+
+bind \cn "nvims"
+###################### NVIM ######################
+
+###################### Git ######################
+# alias gs="git status"
+function gs
+  command git status
+end
+
+# alias gca="git commit --amend"
+function gca
+  command git commit --amend
+end
+
+# Git status
+function gs
+  git status $argv
+end
+
+# Git add all changes
+function ga
+    git add --all $argv
+end
+
+# Git commit with message
+function gc
+    git commit -m $argv
+end
+
+# Git push
+function gp
+    git push $argv
+end
+
+# Git pull
+function gl
+    git pull $argv
+end
+
+# Git branch
+function gb
+    git branch $argv
+end
+
+# Git checkout branch
+function gco
+    git checkout $argv
+end
+
+# Git merge branch
+function gm
+    git merge $argv
+end
+
+# alias glp="git log --oneline --graph --decorate --all"
+function glp
+  command git log --oneline --graph --decorate --all
+end
+###################### Git ######################
+
+###################### EXA ######################
+# alias ls="exa -al --icons --group-directories-first --git"
+function ls
+  command exa -al --icons --group-directories-first --git $argv
+end
+# alias tree="exa -T -L 2 -I 'node_modules|*.pyc'"
+function tree
+  command exa -T -L 2 -I 'node_modules|*.pyc' $argv
+end
+###################### EXA ######################
+
+###################### KUBECTL ######################
+function k
+  command kubectl $argv
+end
+###################### KUBECTL ######################
+
+
--- .zshrc
+++ .zshrc
@@ -2,7 +2,7 @@
 # Fig pre block. Keep at the top of this file.
 [[ -f "$HOME/.fig/shell/zshrc.pre.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.pre.zsh"
 
-eval "$(starship init zsh)"
+# eval "$(starship init zsh)"
 
 
 # set default editor
