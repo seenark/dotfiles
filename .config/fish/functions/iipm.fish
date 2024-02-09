ab75b68eebecc36c047f3e55ecf6c6c17d7fea67
Atiwat Seenark <um_oom@hotmail.com>
2024-02-09 12:41:07 +0700

    add: add fish function for iipm server

---
+++ .config/fish/functions/iipm.fish
@@ -0,0 +1,14 @@
+
+function iipm
+    #!/usr/bin/env fish
+    set -l server_ip "34.126.78.42"
+    # set -l server_pass ""
+    set -l private_key_path "$HOME/.ssh/iipm-deployer"
+    set -l user deployer
+    # set -l ssh_port ""
+
+    # set -l cmd "ssh -o StrictHostKeyChecking=no -i $private_key_path $user@$server_ip"
+    # echo $cmd
+
+    command ssh -o StrictHostKeyChecking=no -i $private_key_path $user@$server_ip
+end
