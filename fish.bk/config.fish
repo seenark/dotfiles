af2910fb34a7d6b37e01a3329b16f475c5f886d1
Atiwat Seenark <um_oom@hotmail.com>
2024-12-29 22:38:40 +0700

    add caraspace

--- fish.bk/config.fish
+++ fish.bk/config.fish
@@ -174,3 +174,10 @@ function plpass
     echo $PROLAB_SERVER_PASSWORD
 end
 ###################### PROLAB SSH ######################
+
+
+# ~/.config/fish/config.fish
+set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
+mkdir -p ~/.config/fish/completions
+carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
+carapace _carapace | source
