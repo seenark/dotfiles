c461e4d4b0bae3b916c151ad6d4c28fdb70dc566
Atiwat Seenark <um_oom@hotmail.com>
2025-01-11 16:11:19 +0700

    update fish fn for private key

--- chezmoi/dot_config/fish/functions/privatekey-chmod-600.fish
+++ chezmoi/dot_config/fish/functions/privatekey-chmod-600.fish
@@ -1,4 +1,5 @@
 function privatekey-chmod-600
-    find ~/.dotfiles/private-key/ -type f ! -name "*.*" -exec chmod 600 {} \;
+    # find ~/.dotfiles/private-key/ -type f ! -name "*.*" -exec chmod 600 {} \;
+    find ~/.private-key/private-key/ -type f ! -name "*.*" -exec chmod 600 {} \;
     echo "Set permissions 600 on private key files"
 end
