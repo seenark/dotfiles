87d7cf21ae85077052e8012956c7d86aaf6a425b
Atiwat Seenark <um_oom@hotmail.com>
2025-10-08 21:52:16 +0700

    update lazy7

--- .config/nvim-lazy7/lua/plugins/my-grapple.lua
+++ .config/nvim-lazy7/lua/plugins/my-grapple.lua
@@ -11,11 +11,11 @@ return {
     keys = {
       { "<c-g>", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
       { ";", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple open tags window" },
-      { "<leader>mn", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
-      { "<leader>mp", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
-      { "<leader>mC", "<cmd>Grapple prune<cr>", desc = "Grapple Prune" },
-      { "<leader>mR", "<cmd>Grapple reset<cr>", desc = "Grapple Reset" },
-      { "<leader>mU", "<cmd>Grapple unload<cr>", desc = "Grapple Unload" },
+      { "<leader>gpn", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
+      { "<leader>gpp", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
+      { "<leader>gpC", "<cmd>Grapple prune<cr>", desc = "Grapple Prune" },
+      { "<leader>gpR", "<cmd>Grapple reset<cr>", desc = "Grapple Reset" },
+      { "<leader>gpU", "<cmd>Grapple unload<cr>", desc = "Grapple Unload" },
     },
   },
 }
