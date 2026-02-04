d7e3a67ddc03bb08a26c6f554f69099c7356442c
Atiwat Seenark <um_oom@hotmail.com>
2026-02-04 12:37:52 +0700

    update skill symlinks

--- .config/fish/functions/skills-symlink.fish
+++ .config/fish/functions/skills-symlink.fish
@@ -1,6 +1,6 @@
 function skills-symlink
     for dir in .agents/skills/*/
         set skill_name (basename $dir)
-        ln -s (pwd)/.agents/skills/$skill_name .claude/skills/$skill_name
+        ln -s ../../.agents/skills/$skill_name .claude/skills/$skill_name
     end
 end
